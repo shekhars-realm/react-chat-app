e9b07c3960b437e96185263fd0610d768864e6e5
Sam <sam@Sams-MacBook-Air.local>
2020-09-13 12:16:59 +0200

    server setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import Join from './components/Join'
+import Chat from './components/Chat'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  return(
+    <Router>
+      <Route path='/' exact component={Join}/>
+      <Route path='/chat' exact component={Chat}/>
+    </Router>
+  )
 }
 
 export default App;
---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
+
+const PORT = process.env.PORT || 5000;
+
+const router = require('./router')
+
+const app = express();
+
+const server = http.createServer(app);
+
+const io = socketio(server);
+
+io.on('connection', (socket) => {
+    console.log('we have a new connection')
+
+    socket.on('disconnect', () => {
+        console.log('user has left');
+    })
+})
+
+app.use(router);
+
+server.listen(PORT, () => {
+    console.log('server has started on port: ', PORT);
+});
+
+
